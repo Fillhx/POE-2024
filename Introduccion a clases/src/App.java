0f84dfdba23a2a987b04c94d9f6821694f28675a
Fillhx <gustavo.restrepo@correounivalle.edu.co>
2024-09-11 22:53:00 -0500

    Hice un codigo y varios archivos, donde aprendi varios conceptos sobre las clases en Java

--- Introduccion a clases/src/App.java
+++ Introduccion a clases/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String universidad = "Del Valle";
+        boolean voyAganarEventos = true;
+        Humano humano1 = new Humano();
+        humano1.setNombre("Adolfo");
+        humano1.setEdad((byte)18);
+        Humano humano2 = new Humano("Gustavo", (byte)20, "1114339186");
+        System.out.println("hola");
+        System.out.println(humano2);
+        System.out.println(humano2.getNombre());
+        System.out.println(humano1.getNombre());
+        System.out.println(humano1.getEdad());
+        humano1.saludar();
+        humano2.saludar();
+        humano1.presentarse();
+        humano2.presentarse();
+        String estratoAdolfo = humano1.estrato(2000000);
+        System.out.println(estratoAdolfo);
+
+
+
     }
 }
